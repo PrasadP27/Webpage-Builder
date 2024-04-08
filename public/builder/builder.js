93c5b5f662522c642f09e6768e187001fdc328da
PrasadP27 <bobpanchal948@gmail.com>
2024-04-09 00:45:57 +0530

    Added Top-Navbar

--- public/builder/builder.js
+++ public/builder/builder.js
@@ -1,6 +1,94 @@
 const editor = grapesjs.init({
-    container: '#editor',
-    fromElement: true,
-    storageManager: false,
-    panels: { defaults: {} },
-  });
+  container: '#editor',
+  height: '100%',
+  width: '100%',
+  fromElement: true,
+  storageManager: false,
+
+  panels: {
+    defaults: [
+      {
+        id: "panel-devices",
+        el: ".panel_devices",
+        buttons: [
+          {
+            id: "device-desktop",
+            label: '<img src="assets/icons/desktop.svg" alt="desktop">',
+            command: "set-device-desktop",
+            active: true,
+            togglable: false,
+          },
+          {
+            id: 'device-tablet',
+            label: '<img src="assets/icons/tablet.svg" alt="tablet">',
+            command: 'set-device-tablet',
+            togglable: false,
+          },
+          {
+            id: "device-mobile",
+            label: '<img src="assets/icons/mobile.svg" alt="mobile">',
+            command: "set-device-mobile",
+            togglable: false
+          }
+        ]
+      },
+      {
+        id: "panel-basic-actions",
+        el: ".panel_basic_actions",
+        buttons: [
+          {
+            id: "visibility",
+            active: true,
+            className: "btn-toggle-borders",
+            label: '<img src="assets/icons/view-components.svg" alt="code">',
+            command: "sw-visibility",
+          },
+          {
+            id: 'export',
+            className: 'btn-open-export',
+            label: '<img src="assets/icons/code.svg" alt="code">',
+            command: 'export-template',
+            context: 'export-template',
+          }
+        ]
+      }
+    ]
+  },
+
+  deviceManager: {
+    devices: [
+      {
+        name: "Desktop",
+        width: ""
+      },
+      {
+        name: "Tablet",
+        width: "768px",
+        widthMedia: "992px"
+      },
+      {
+        name: "Mobile",
+        width: "320px",
+        widthMedia: "480px"
+      }
+    ]
+  },
+
+});
+
+
+//for devices
+editor.Commands.add('set-device-desktop', {
+  run: (editor) => editor.setDevice('Desktop'),
+});
+
+editor.Commands.add('set-device-tablet', {
+  run: (editor) => editor.setDevice('Tablet'),
+});
+
+editor.Commands.add('set-device-mobile', {
+  run: (editor) => editor.setDevice('Mobile'),
+});
+
+// Set the initial device
+editor.setDevice('Desktop');
--- public/builder/builderCustom.js
+++ public/builder/builderCustom.js
@@ -1,27 +1,21 @@
+// for Element-navbar 
 const tabButtons = document.querySelectorAll('#element-navbar button');
 
-// Add click event listener to each tab button
 tabButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        // Remove active class from all tab buttons
         tabButtons.forEach((b) => b.classList.remove('active'));
 
-        // Add active class to the clicked tab button
         button.classList.add('active');
 
-        // Get the target tab content
         const targetTabContent = document.querySelector(button.getAttribute('data-bs-target'));
 
-        // Get all tab content
         const tabContents = document.querySelectorAll('#element #element-content .tab-pane');
 
-        // Hide all tab content
         tabContents.forEach((content) => {
             content.classList.remove('show');
             content.classList.add('hidden');
         });
 
-        // Show the target tab content
         targetTabContent.classList.remove('hidden');
         targetTabContent.classList.add('show');
     });
