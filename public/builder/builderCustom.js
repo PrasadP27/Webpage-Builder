2c2b4e2b388d9a6abd4f5dad390c252db5863312
PrasadP27 <bobpanchal948@gmail.com>
2024-04-11 21:46:07 +0530

    Added hide left-navbar

--- public/builder/builderCustom.js
+++ public/builder/builderCustom.js
@@ -21,21 +21,52 @@ tabButtons.forEach((button) => {
     });
 });
 
-// for elements
-
+// for left nav
 const xbtn = document.querySelector('.xbtn')
 const element = document.querySelector("#element")
+const leftNav = document.querySelector('#left-navbar')
+const main = document.querySelector('#main-content')
 
 xbtn.onclick = () => {
-    element.classList.add('hidden')
-    element.classList.remove('show')
+
+    const hideLeftNav = gsap.timeline();
+    hideLeftNav.to(element, {
+        xPercent: -150,
+        opacity: 0,
+        visibility: 'hidden'
+    })
+    hideLeftNav.to(leftNav, {
+        width: '0%',
+        minWidth: '0px',
+        delay: -0.5
+    })
+    hideLeftNav.to(main, {
+        width: 'calc(100% - 50px)',
+        left: '50px',
+        delay: -0.5
+    })
 
     tabButtons.forEach((button) => {
         button.addEventListener('click', () => {
-            element.classList.remove('hidden')
-            element.classList.add('show');
+
+            const showLeftNav = gsap.timeline();
+            showLeftNav.to(element, {
+                xPercent: 0,
+                opacity: 1,
+                visibility: 'visible'
+            })
+            showLeftNav.to(leftNav, {
+                width: '20%',
+                minWidth: '300px',
+                delay: -0.5
+            })
+            showLeftNav.to(main, {
+                width: '80%',
+                left: '20%',
+                delay: -0.5
+            })
         });
-        // for element-navbar 
+        // for element-navbar
         button.classList.remove('active')
-    }); 
+    });
 }
