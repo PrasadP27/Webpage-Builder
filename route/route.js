56d19d62cc0038d12952b80e17cc8ab1464914f5
PrasadP27 <bobpanchal948@gmail.com>
2024-04-13 08:42:13 +0530

    Added Home Page

--- route/route.js
+++ route/route.js
@@ -2,10 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 //home page
+const homeHeadTags = [`
+    <link rel="stylesheet" href="/home/home.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js" integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+`]
+
+const homeBodyTags = [`
+    <script src="/builder/builderCustom.js"></script>
+    <script src="/builder/builder.js"></script>
+`]
 router.get("/", (req, res) => {
     res.render("home", {
         title: null,
-        headTags: null,
+        headTags: homeHeadTags,
         bodyTags: null
     })
 })
